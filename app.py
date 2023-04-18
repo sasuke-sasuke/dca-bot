df2304f447b59221022f049501f6085a3bdf76aa
Jeof Petty <jeofpetty@icloud.com>
2023-04-17 23:34:21 -0400

    init commit

---
+++ app.py
@@ -0,0 +1,10 @@
+import time
+import warnings
+import numpy as np
+import pandas as pd
+import datetime as dt
+import matplotlib.pyplot as plt
+import alpaca_trade_api as tradeapi
+from alpaca_trade_api.rest import TimeFrame
+warnings.filterwarnings("ignore", category=FutureWarning)
+
